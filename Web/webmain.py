fabebbf847013327f6a93ea0041f826d380ad217
Te2337 <91050431+Te2337@users.noreply.github.com>
2024-04-23 00:02:06 -0400

    Update webmain.py

--- Web/webmain.py
+++ Web/webmain.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from Web.danchan_predict import danchan_predict
+from danchan_predict import danchan_predict
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def index():
     if request.method == 'POST':
         # 从表单中获取用户输入的地名和年份
         area = request.form['area']
-        year = request.form['year']
+        year = int(request.form['year'])
 
         # 调用函数计算系数
         danchan = danchan_predict(area, year)
